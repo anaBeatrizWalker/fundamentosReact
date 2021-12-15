ad5936015c294da8548225f07254b5daa496bc0f
Ana Beatriz <anabiawalker@gmail.com>
2021-12-15 15:46:27 -0300

    Componente enxuto

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -5,16 +5,16 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 
-export default function App(){
-    return (
-        <div id="app">
-            <h1>Fundamentos React</h1>
-            <Fragmento />
-            <ComParametro 
-                titulo="Situação do Aluno" 
-                aluno="Pedro" 
-                nota={9.5}/>
-            <Primeiro></Primeiro>
-        </div>
-    )
-}
+export default _ =>//
+    <div id="app">
+        <h1>Fundamentos React</h1>
+        <Fragmento />
+        <ComParametro
+            titulo="Situação do Aluno"
+            aluno="Pedro"
+            nota={9.5} />
+        <Primeiro></Primeiro>
+    </div>
+/*Obs: 
+underline: função de apenas um parametro não obrigatório;
+as chaves podem ser retiradas junto com o return*/
