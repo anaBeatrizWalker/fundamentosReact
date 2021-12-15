b0047b8a53146c83b1ba40860609fefdbdf9438c
Ana Beatriz <anabiawalker@gmail.com>
2021-12-15 14:18:13 -0300

    Adicionando ReactDOM

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom' //importando o React
+
+const el = document.getElementById('root')
+
+ReactDOM.render('Olá react!', el )//renderizando mensagem na div com id=root no arquivo index.html
