c36cb4a36e6154ded2217331e2a7d7a103583ee2
Ana Beatriz <anabiawalker@gmail.com>
2021-12-15 15:08:31 -0300

    Adicionando Componente com parâmetro

---
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function ComParametro(props){
+    return (
+        <div>
+            <h2>{ props.titulo }</h2>
+            <h3>{ props.subtitulo }</h3>
+        </div>
+    )
+}
+/*Obs: props (propriedade) se torna um objeto
+{Titulo: "Segundo Componente, Subtitulo: "Com parametros"}
+*/
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,11 +2,15 @@ import './index.css' //caminho relativo (arquivo na mesma pasta)
 import ReactDOM from 'react-dom' //importando o React
 import React from 'react'//importando React para usar o JSX
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
     //referenciando o arquivo como tag
     <div>
         <Primeiro></Primeiro>
+        <ComParametro 
+            titulo="Segundo Componente" 
+            subtitulo="Com paramêtros" />
     </div>, 
     document.getElementById('root')
 )
